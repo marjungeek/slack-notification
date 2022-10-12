49eb4d102fba99dde205961c0b170c7496aa891d
Marjun F. Villegas <marjunvillegas.geek@gmail.com>
2022-10-12 22:43:16 +0800

    new update

--- index.js
+++ index.js
@@ -3,20 +3,16 @@ const github = require('@actions/github');
 const axios = require('axios');
 
 const SLACK_URL = core.getInput('slack_url');
-const slackAction = async () => {
+const message = core.getInput('message');
 
-    //{\"channel\": \"#testing-alert\", \"username\": \"webhookbot\", \"text\": \"This is posted to #testing-alert and comes from a bot named webhookbot.\", \"icon_emoji\": \":ghost:\"}"
+const slackAction = async () => {
+    
     try{
         const json = JSON.stringify({ 
-            channel: "#testing-alerts", 
-            username: "bot",
-            icon_emoji: ":ghost:",
-            text : "GitHub Action build result: ${{ job.status }}\n${{ github.event.pull_request.html_url || github.event.head_commit.url }}"
-
+            text : message
         });
         await axios.post(SLACK_URL, json, {
             headers: {
-                // Overwrite Axios's automatically set Content-Type
                 'Content-Type': 'application/json'
             }
         });
