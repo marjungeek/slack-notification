88b7766038a53a3a9f775fa15b9625d940ab5ebe
Marjun F. Villegas <marjunvillegas.geek@gmail.com>
2022-10-12 19:56:17 +0800

    new update

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+import SlackNotify from 'slack-notify';
+const MY_SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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';
+const slack = SlackNotify(MY_SLACK_WEBHOOK_URL);
+
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
@@ -10,6 +14,15 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+
+  slack.send({
+    channel: '#test-alert',
+    icon_url: 'http://example.com/my-icon.png',
+    text: 'Here is my notification',
+    unfurl_links: 1,
+    username: 'Jimmy'
+  });
+
 } catch (error) {
   core.setFailed(error.message);
 }
