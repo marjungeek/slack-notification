4cd9b28a1281ee87b62d1a9c12d35cbf5a1de988
Marjun F. Villegas <marjunvillegas.geek@gmail.com>
2022-10-13 12:45:08 +0800

    my new update

--- index.js
+++ index.js
@@ -9,8 +9,40 @@ const slackAction = async () => {
     
     try{
         const json = JSON.stringify({ 
-            text : message
+            text : message,
+            blocks: [
+                {
+                    type: "header",
+                    text: {
+                        type: "plain_text",
+                        text: "New request",
+                        emoji: true
+                    }
+                },
+                {
+                    type: "section",
+                    fields: [
+                        {
+                            type: "mrkdwn",
+                            text: "*Type:*\nPaid Time Off"
+                        },
+                        {
+                            type: "mrkdwn",
+                            text: "*Created by:*\n<example.com|Fred Enriquez>"
+                        }
+                    ]
+                },
+                {
+                    type: "section",
+                    fields: [
+                        {
+                            type: "mrkdwn",
+                            text: "*When:*\nAug 10 - Aug 13"
+                        }
+                    ]
+                }]
         });
+        
         await axios.post(SLACK_URL, json, {
             headers: {
                 'Content-Type': 'application/json'
