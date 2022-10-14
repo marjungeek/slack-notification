77efc24984548787dc2d5aeac11830da0cbb91d3
Marjun F. Villegas <marjunvillegas.geek@gmail.com>
2022-10-14 09:33:25 +0800

    new update

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ const slackAction = async () => {
                     fields: [
                         {
                             type: "mrkdwn",
-                            text: `| <${core.getInput('message')}|View Commit> | <${action_url}|View Build> | ` + JSON.stringify(github.context.payload, undefined, 2)
+                            text: JSON.stringify(github.context.payload, undefined, 2)
                         }
                     ]
                 }
